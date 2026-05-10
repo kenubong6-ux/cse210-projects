87beec8316c9452b2db731db5e63aff01a714761
kenubong6-ux <kubong@byupathway.edu>
2026-05-10 14:06:04 +0100

    WEEK 1 COMPLETED

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+
+        //Get the user's name and save it in a variable
+        string userName = PromptUserName();
+
+        //Get the user's number and save it in a variable
+        int userNumber = PromptUserNumber();
+
+        //Pass the number into the squaring function and save the result
+        int squaredNumber = SquareNumber(userNumber);
+
+        //Pass both the name and the squared result into the display function
+        DisplayResult(userName, squaredNumber);
+    }
+
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string input = Console.ReadLine();
+        int number = int.Parse(input);
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult(string name, int squaredNumber)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
     }
 }
